43bcb9b78db4b8a395889fe416af7b39f1debb43
Anael Medeiros <anael.medeiros@compasso.com.br>
2022-03-23 15:56:46 -0400

    envfile

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_0jH2OeCsIFiiXJ6yk4TxVs4PG5lBax0",
-  authDomain: "referee-88cd0.firebaseapp.com",
-  projectId: "referee-88cd0",
-  storageBucket: "referee-88cd0.appspot.com",
-  messagingSenderId: "657734988054",
-  appId: "1:657734988054:web:ed348a51beef4227f1a037",
-  measurementId: "G-Z7VK1NEES1",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
